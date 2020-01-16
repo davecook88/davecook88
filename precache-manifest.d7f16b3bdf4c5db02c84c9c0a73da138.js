b5719096dad17b8036a708e8d2eb79123c66c0c9
Dave Cook <DaveCook@hotmail.co.uk>
2020-01-16 19:11:18 +0700

    Updates

---
+++ precache-manifest.d7f16b3bdf4c5db02c84c9c0a73da138.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f21775fe2efa81b526c764f4dad7092",
+    "url": "/davecook88/index.html"
+  },
+  {
+    "revision": "41cc6ea070b87d5013ce",
+    "url": "/davecook88/static/css/main.5a280965.chunk.css"
+  },
+  {
+    "revision": "603124202db8fa9226e8",
+    "url": "/davecook88/static/js/2.4b7a4d1d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/davecook88/static/js/2.4b7a4d1d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41cc6ea070b87d5013ce",
+    "url": "/davecook88/static/js/main.0c443ae3.chunk.js"
+  },
+  {
+    "revision": "fecbf149ff8dbdaa38ed",
+    "url": "/davecook88/static/js/runtime-main.f2c5e2c9.js"
+  },
+  {
+    "revision": "8c225c3eb15a8f07ebee6110e377bd38",
+    "url": "/davecook88/static/media/photo.8c225c3e.png"
+  }
+]);
