e0414714f27b1de623b83f0f47d758c2f1189f46
Dave Cook <DaveCook@hotmail.co.uk>
2020-03-31 11:57:45 +0700

    Updates

---
+++ precache-manifest.507aefff09e65183285544aaacc1f497.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff93acf7f3f06f9dc73f15ef22af2c90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e00d943fc195b66a7624",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "b7f69b2cd8e934c4854d",
+    "url": "/static/css/main.36bb9b0c.chunk.css"
+  },
+  {
+    "revision": "e00d943fc195b66a7624",
+    "url": "/static/js/2.e977bca7.chunk.js"
+  },
+  {
+    "revision": "549cff9698f0d47a2a839df6aef79c2b",
+    "url": "/static/js/2.e977bca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f69b2cd8e934c4854d",
+    "url": "/static/js/main.3b0aaa5f.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "55f2ea90a16658f4750c9112c10c3561",
+    "url": "/static/media/lakarencita.55f2ea90.PNG"
+  },
+  {
+    "revision": "9b0a099a11157c7e1094dcc5471fba57",
+    "url": "/static/media/snakegif.9b0a099a.gif"
+  }
+]);
