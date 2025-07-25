71308f69864b221bbccd37cdc275541bc9856553
David Cook <davecook@hotmail.co.uk>
2025-07-25 09:50:49 -0600

    reorganized icons

--- src/components/EditorLayout/Files.tsx
+++ src/components/EditorLayout/Files.tsx
@@ -1,11 +1,20 @@
+import { JSONFileLogo } from "#/components/Logos/json-file";
 import { MenuSection } from "./MenuSection";
 
 export const FilesSection = () => {
   return (
     <MenuSection title="Dave Cook">
       <div className="p-2 flex flex-col space-y-2 text-sm">
-        <div>logo</div>
-        <div>me.json</div>
+        <a
+          href="https://github.com/davecook88"
+          class="flex items-center space-x-2 text-gray-400 hover:text-white transition"
+          aria-label="GitHub"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <JSONFileLogo />
+          <span>me.json</span>
+        </a>
       </div>
     </MenuSection>
   );
